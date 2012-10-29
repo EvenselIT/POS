72849bfa242461c9a1aa27a561c0214b6f7560a1
Evensel-IT <contact@evenselit.com>
2012-10-29 14:38:07 +0530

    added inventory manager

---
+++ EvenselRetailSystem/RetailService/InventoryManager.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RetailService.Framework;
+
+namespace Evensel.RetailService
+{
+    public class InventoryManager: AbstractManager<Inventory>
+    {
+        public override int AddNew(Inventory obj)
+        {
+            using (EvenselPOSEntities context = new EvenselPOSEntities())
+            {
+                context.AddToInventories(obj);
+                context.SaveChanges();
+            }
+            return 1;
+        }
+
+        public override List<Inventory> SelectAll()
+        {
+            using (EvenselPOSEntities context = new EvenselPOSEntities())
+            {
+                var query = from n in context.Inventories
+                            select n;
+
+                if (query != null)
+                {
+                    return query.ToList();
+                }
+            }
+
+            return null;
+        }
+
+        public override Inventory GetByID(int ID)
+        {
+            using (EvenselPOSEntities context = new EvenselPOSEntities())
+            {
+                var query = from n in context.Inventories
+                            where n.ID == ID
+                            select n;
+
+                if (query != null)
+                {
+                    return query.SingleOrDefault();
+                }
+            }
+
+            return null;
+        }
+
+        public override void Delete(int ID)
+        {
+            using (EvenselPOSEntities context = new EvenselPOSEntities())
+            {
+                var query = from n in context.Inventories
+                            where n.ID == ID
+                            select n;
+
+                if (query != null)
+                {
+                    var selectedObject = query.SingleOrDefault();
+                    context.DeleteObject(selectedObject);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        /// <summary>
+        /// update inventory, set last update date as datetime now.
+        /// </summary>
+        /// <param name="obj"></param>
+        public override void Update(Inventory obj)
+        {
+            using (EvenselPOSEntities context = new EvenselPOSEntities())
+            {
+                var query = from n in context.Inventories
+                            where n.ID == obj.ID
+                            select n;
+
+                if (query != null)
+                {
+                    var selectedObject = query.SingleOrDefault();
+
+                    selectedObject.Location = obj.Location;
+                    selectedObject.QtyOnHand = obj.QtyOnHand;
+                    selectedObject.ReOrderLevel = obj.ReOrderLevel;
+                    selectedObject.LastUpdateDate = DateTime.Now;
+
+
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
