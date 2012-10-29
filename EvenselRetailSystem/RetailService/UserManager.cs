13d52e19c1388a75249b164276e16b31996cf740
Evensel-IT <contact@evenselit.com>
2012-10-29 13:54:27 +0530

    User manage class added
    User manage class added

---
+++ EvenselRetailSystem/RetailService/UserManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RetailService.Framework;
+
+namespace RetailService
+{
+    class UserManager : AbstractManager <User>
+    {
+        public override int AddNew(User obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<User> SelectAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override User GetByID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete(int ID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
